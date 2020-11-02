9c8793ddab14c8e3fe2b0a239f9e02f23427f2f2
Geoneun <ddolbangpark@naver.com>
2020-11-03 00:22:13 +0900

    [Updated] check [GET], initializing

--- server/server.py
+++ server/server.py
@@ -7,7 +7,10 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+print("Connecting Arudino...")
 time.sleep(1)
+print("Connected.")
+pinr("============")
 
 app = Flask(__name__)
 
@@ -20,7 +23,11 @@ def setLed():
     str = 'L{0:0>3}{1:0>3}{2:0>3}'.format(r,g,b)
     print(str)
     ser.write(bytes(str))
-    return "Hello."
+    return 'LED Setted R:{0}, G:{1}, B{2}'.format(r,g,b)
+
+@app.route('/check', mothods=['GET'])
+def checkConn():
+    return "ok"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
